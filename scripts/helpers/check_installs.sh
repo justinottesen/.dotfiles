90a7738a8feaa32afcdd9f3ae1445c835ee5368a
Justin Ottesen <justinottesen@gmail.com>
2025-03-22 21:27:50 -0400

    Add .zshrc

--- scripts/helpers/check_installs.sh
+++ scripts/helpers/check_installs.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 # OS="$1" # Checked in 'setup.sh', assumed to be valid here, maybe used later
 
+# Commands which can be checked with a simple 'command -v' call
 declare -a PROGRAMS=(
   # Required for setup
   "git"
@@ -28,3 +29,10 @@ for prog in "${PROGRAMS[@]}"
 do
   command -v "$prog" >/dev/null 2>&1 || { echo >&2 "$prog is not installed. Aborting."; exit 1; }
 done
+
+# Other installs which need to be specially checked
+
+# Oh My Zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+  echo "Oh My Zsh is not installed. Aborting."; exit 1;
+fi
