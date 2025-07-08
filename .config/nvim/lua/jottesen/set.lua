f19a2d985e18f7ecc585c07c7f66bc2fd7b64c0a
Justin Ottesen <justinottesen@gmail.com>
2025-07-08 16:35:36 -0400

    Use <C-_> directly to move windows instead of <C-w>_

--- .config/nvim/lua/jottesen/set.lua
+++ .config/nvim/lua/jottesen/set.lua
@@ -9,6 +9,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.autoindent = true
 
 -- Save undo history
 vim.opt.undofile = true
@@ -17,3 +18,12 @@ vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Use system clipboard for copy and paste
+vim.opt.clipboard = "unnamedplus"
+
+-- Windowing settings
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
