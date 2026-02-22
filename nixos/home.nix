c4c7b0a2948ba8e6a020299fed15ea46bca1b18a
Justin Ottesen <justinottesen@gmail.com>
2026-02-22 11:42:08 -0500

    Use stow for dotfiles

--- nixos/home.nix
+++ nixos/home.nix
@@ -2,28 +2,28 @@
 
 let
   repo = "${config.home.homeDirectory}/nixos-dotfiles";
-  dotfiles = "dotfiles";
 in {
   home = {
     username = "justin";
     homeDirectory = "/home/justin";
-    packages = with pkgs; [
+  };
+
+  # Instead of using nix to configure everything and locking us into NixOS, we
+  # use regular dotfiles for configuring.
+  home.activation.stowDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    set -euo pipefail
+    export STOW_BIN="${pkgs.stow}/bin/stow"
+    ${pkgs.bash}/bin/bash ${toString repo}/scripts/stow-dotfiles
+  '';
+
+  # We still use nixos for the packages we install for reproducibility
+  home.packages = with pkgs; [
       git
       neovim
       stow
     ];
   };
-
-  home.activation.stowDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    set -euo pipefail
-
-    ${pkgs.stow}/bin/stow \
-      --dir "${toString repo}" \
-      --target "$HOME" \
-      --restow \
-      "${toString dotfiles}"
-  '';
-
+  
   # DO NOT CHANGE - see configuration.nix
   home.stateVersion = "25.11";
 }
