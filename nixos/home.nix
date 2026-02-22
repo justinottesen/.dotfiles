fda549a5318a38d7ac29e5ce696120810785f0e2
Justin Ottesen <justinottesen@gmail.com>
2026-02-22 15:10:11 -0500

    Put symlinking scripts into home.nix

--- nixos/home.nix
+++ nixos/home.nix
@@ -10,11 +10,19 @@ in {
 
   # Instead of using nix to configure everything and locking us into NixOS, we
   # use regular dotfiles for configuring.
-  home.activation.stowDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    set -euo pipefail
-    export STOW_BIN="${pkgs.stow}/bin/stow"
-    ${pkgs.bash}/bin/bash ${toString repo}/scripts/stow-dotfiles
-  '';
+  home.activation = {
+    # Stow the dotfiles from this repo to their expected paths
+    stowDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      set -euo pipefail
+      export STOW_BIN="${pkgs.stow}/bin/stow"
+      ${pkgs.bash}/bin/bash ${toString repo}/scripts/stow-dotfiles
+    '';
+    # Symlink the scripts dir into the .local/bin dir
+    symlinkScripts = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      set -euo pipefail
+      ${pkgs.bash}/bin/bash ${toString repo}/scripts/symlink-scripts
+    '';
+  };
 
   # We still use nixos for the packages we install for reproducibility
   home.packages = with pkgs; [
