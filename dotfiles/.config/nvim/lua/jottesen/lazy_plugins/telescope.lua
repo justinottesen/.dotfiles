cad9a242d85537f051aae01ede6f1e54378f3e55
Justin Ottesen <justinottesen@gmail.com>
2026-02-23 18:53:55 -0500

    Improve telescope file search

--- dotfiles/.config/nvim/lua/jottesen/lazy_plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/jottesen/lazy_plugins/telescope.lua
@@ -16,9 +16,42 @@ return {
         require("telescope").load_extension("ui-select")
 
         local builtin = require("telescope.builtin")
+
+        -- Searches everything, all files
+        vim.keymap.set("n", "<leader>se", function()
+                builtin.find_files {
+                    hidden = true,
+                    no_ignore = true,
+                    no_ignore_parent = true,
+                }
+            end, { desc = "[S]earch [E]verything" })
+
+        local most_finder = function()
+            builtin.find_files {
+                hidden = true,
+                no_ignore = true,
+                no_ignore_parent = true,
+                file_ignore_patterns = {
+                    "%.git/",
+                }
+            }
+        end
+
+        -- Searches most, has some typical defaults to ignore
+        vim.keymap.set("n", "<leader>sm", most_finder, { desc = "[S]earch [M]ost" })
+
+        -- Search Files (expected default); respect .gitignore, fall back to most outside git repos
+         vim.keymap.set("n", "<leader>sf", function()
+                local ok = pcall(builtin.git_files, {
+                    show_untracked = true,
+                })
+                if not ok then
+                    most_finder()
+                end
+            end, { desc = "[S]earch [F]iles" })
+
         vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
         vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-        vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
         vim.keymap.set("n", "<leader>ss", builtin.find_files, { desc = "[S]earch [S]elect Telescope" })
         vim.keymap.set({ "n", "v" }, "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
         vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
