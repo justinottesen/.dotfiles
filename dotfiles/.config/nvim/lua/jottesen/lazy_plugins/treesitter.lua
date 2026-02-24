9b19a1edcc42bb1b350bbd9aa70b23fd66d560e6
Justin Ottesen <justinottesen@gmail.com>
2026-02-24 07:59:01 -0500

    Fix treesitter config

--- dotfiles/.config/nvim/lua/jottesen/lazy_plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/jottesen/lazy_plugins/treesitter.lua
@@ -1,17 +1,13 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
-    build = ':TSUpdate',
-    opts = {
-        highlight = { enable = true },
-        indent = { enable = true},
-        autotage = { enable = true},
-        ensure_installed = {
-            "c",
-            "cpp",
-            "go",
-            "lua",
-        },
-        auto_install = false,
-    },
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter").setup({
+            install_dir = vim.fn.stdpath("data") .. "/site",
+        })
+
+        -- Install parsers (async). First run will download + compile.
+        require("nvim-treesitter").install({ "c", "cpp", "go", "lua", "nix" })
+    end,
 }
