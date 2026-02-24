e4d106691384e475f464290de52545ff7504fd53
Justin Ottesen <justinottesen@gmail.com>
2026-02-23 19:43:50 -0500

    Add neovim lsp config

---
+++ dotfiles/.config/nvim/lua/jottesen/lazy_plugins/lsp.lua
@@ -0,0 +1,42 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        { "j-hui/fidget.nvim", opts = {} },
+    },
+    config = function()
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+            callback = function(event)
+                local map = function(keys, func, desc, mode)
+                    mode = mode or "n"
+                    vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+                end
+
+                map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+				map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+				map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+				map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+				map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+				map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+				map("gO", require("telescope.builtin").lsp_document_symbols, "[O]pen Document Symbols")
+				map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open [W]orkspace Symbols")
+				map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+
+                local client = vim.lsp.get_client_by_id(event.data.client_id)
+                if client and client:supports_method("textDocument/inlayHind", event.buf) then
+                    map("<leader>th", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end, "[T]oggle Inlay [H]ints")
+                end
+            end,
+        })
+
+        vim.diagnostic.config({
+            severity_sort = true,
+            virtual_text = true,
+        })
+
+        -- Enable different LSPs
+        vim.lsp.enable("lua_ls")
+        vim.lsp.enable("clangd")
+
+    end,
+}
