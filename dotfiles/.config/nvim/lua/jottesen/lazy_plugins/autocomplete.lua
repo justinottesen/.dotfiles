7b57436f78c1fa2aea84bd216fcf8730b38a1e34
Justin Ottesen <justinottesen@gmail.com>
2026-02-23 23:25:56 -0500

    Fix tab jumps from auto complete

--- dotfiles/.config/nvim/lua/jottesen/lazy_plugins/autocomplete.lua
+++ dotfiles/.config/nvim/lua/jottesen/lazy_plugins/autocomplete.lua
@@ -17,5 +17,24 @@ return {
         snippets = { preset = "luasnip" },
         fuzzy = { implementation = "lua" },
         signature = { enabled = true },
+
+        keymap = {
+            ["<C-y"] = { "accept" },
+            ["<Tab>"] = { "snippet_forward", "fallback" },
+            ["<S-Tab>"] = { "snippet_backward", "fallback" },
+        },
     },
+    config = function(_, opts)
+        require("blink.cmp").setup(opts)
+
+        vim.api.nvim_create_autocmd({ "InsertLeave", "CursorMovedI" }, {
+            callback = function()
+                local ok, ls = pcall(require, "luasnip")
+                if not ok then return end
+                if ls.in_snippet() then
+                    ls.unlink_current()
+                end
+            end,
+        })
+    end,
 }
