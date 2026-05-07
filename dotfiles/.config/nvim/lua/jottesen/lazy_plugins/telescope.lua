0b73f5f7749469efe1f06e0d394317dba050f5bd
Justin Ottesen <justinottesen@gmail.com>
2026-05-07 09:29:09 -0400

    Add telescope command sC to search Config

--- dotfiles/.config/nvim/lua/jottesen/lazy_plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/jottesen/lazy_plugins/telescope.lua
@@ -60,6 +60,7 @@ return {
         vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
         vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = "[S]earch Recent Files ('.' for repeat)" })
         vim.keymap.set("n", "<leader>sc", builtin.commands, { desc = "[S]earch [C]ommands" })
+        vim.keymap.set("n", "<leader>sC", function() builtin.git_files { show_untracked = true, cwd = "~/nixos-dotfiles/" } end, { desc = "[S]earch [C]config" })
         vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
         vim.keymap.set("n", "<leader>s/", function()
                 builtin.live_grep {
